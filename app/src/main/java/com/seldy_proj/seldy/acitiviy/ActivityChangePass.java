d0a6c1f86b857445ba278ea96ecb698ca1af4cbc
BeomSoo Park <linoleca@naver.com>
2021-08-26 02:06:29 +0900

    changepass

--- app/src/main/java/com/seldy_proj/seldy/acitiviy/ActivityChangePass.java
+++ app/src/main/java/com/seldy_proj/seldy/acitiviy/ActivityChangePass.java
@@ -1,9 +1,13 @@
 package com.seldy_proj.seldy.acitiviy;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,20 +16,29 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.seldy_proj.seldy.R;
 import com.seldy_proj.seldy.util.Emailcheck;
 
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.mail.MessagingException;
 import javax.mail.SendFailedException;
+
+import static android.content.ContentValues.TAG;
 
 public class ActivityChangePass extends AppCompatActivity {
 
@@ -60,7 +73,6 @@ public class ActivityChangePass extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
 
-
         id = findViewById(R.id.register_id);
         pw = findViewById(R.id.register_pw);
         pwc = findViewById(R.id.register_pwc);
@@ -78,35 +90,118 @@ public class ActivityChangePass extends AppCompatActivity {
         change_btn = findViewById(R.id.change);
         email_check_btn = findViewById(R.id.email_check);
 
+        //비밀번호 변경
         change_btn.setOnClickListener(new View.OnClickListener() {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            String newPassword;
             @Override
             public void onClick(View v) {
                 // 가입 정보 가져오기
-                String getUserId = id.getText().toString();
                 String getUserPw = pw.getText().toString();
                 String getUserPwc = pwc.getText().toString();
-            }
-        });
+
+                if (getUserPw.equals("") != true && getUserPwc.equals("") != true) {
+                    if (pw_c != true || pwc_c != true) {
+                        Toast.makeText(ActivityChangePass.this, "비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        newPassword = pw.getText().toString().trim();
+                        user.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "User password updated.");
+
+                                    Toast.makeText(ActivityChangePass.this, "비밀번호가 변경되었습니다.", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(ActivityChangePass.this, ActivityLogin.class);
+                                    startActivity(intent);
+                                }
+                            }
+                        });
+
+/*
+                        mAuth.createUserWithEmailAndPassword(getUserId, getUserPw).addOnCompleteListener(ActivityChangePass.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    String userId = user.getEmail();
+                                    String uid = user.getUid();
+
+                                    HashMap<Object, String> saveProfile = new HashMap<>();
+                                    saveProfile.put("uid", uid);
+                                    saveProfile.put("id", userId);
+
+                                    mDatabase.child("user").child(uid).setValue(saveProfile);
+
+                                    //가입이 되었으면 로그인 페이지로 --> 수정했으면 로그아웃
+                                    Toast.makeText(ActivityChangePass.this, "비밀번호가 변경되었습니다.", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(ActivityChangePass.this, ActivityLogin.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(ActivityChangePass.this, "이미 존재하는 아이디입니다.", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+                        });*/
+                    }
 
 
-        // 이메일 인증 코드
-        email_check_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    Emailcheck emailsend = new Emailcheck("seldybase777@gmail.com", "!tpfel123");
-                    emailsend.sendMail("Seldy 인증코드 입니다.", emailsend.getEmailCode(), id.getText().toString());
-                    code = emailsend.getEmailCode();
-                    Toast.makeText(getApplicationContext(), "이메일 인증코드를 발송하였습니다.", Toast.LENGTH_SHORT).show();
-                } catch (SendFailedException e) {
-                    Toast.makeText(getApplicationContext(), "이메일 형식이 잘못되었습니다.", Toast.LENGTH_SHORT).show();
-                } catch (MessagingException e) {
-                    Toast.makeText(getApplicationContext(), "인터넷 연결을 확인해주십시오", Toast.LENGTH_SHORT).show();
-                } catch (Exception e) {
-                    e.printStackTrace();
+                } else {
+                    Toast.makeText(ActivityChangePass.this, "공백없이 입력해주세요.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
+
+        //이메일 인증
+        email_check_btn.setOnClickListener(new View.OnClickListener() {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+            @Override
+            public void onClick(View v) {
+                //현재 로그인한 사용자 프로필 가져오기
+                if (user != null) {
+                    String email = user.getEmail().trim();
+                    //Log.e("아무거나",email);
+                    if (id.getText().toString().equals(email)){
+                        //Toast.makeText(ActivityChangePass.this, "이메일 인증코드를 발송하였습니다.", Toast.LENGTH_SHORT).show();
+                        //이메일 인증코드
+                        try {
+                            Emailcheck emailsend = new Emailcheck("seldybase777@gmail.com", "!tpfel123");
+                            AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+                                @Override
+                                protected Void doInBackground(Void... voids) {
+                                    try {
+                                        emailsend.sendMail("Seldy 인증코드 입니다.", emailsend.getEmailCode(), id.getText().toString());
+                                    } catch (SendFailedException e) {
+                                        Toast.makeText(getApplicationContext(), "이메일 형식이 잘못되었습니다.", Toast.LENGTH_SHORT).show();
+                                    } catch (MessagingException e) {
+                                        Toast.makeText(getApplicationContext(), "인터넷 연결을 확인해주십시오", Toast.LENGTH_SHORT).show();
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    return null;
+                                }
+
+                                @Override
+                                protected void onPostExecute(Void unused) {
+                                    super.onPostExecute(unused);
+                                    Toast.makeText(getApplicationContext(), "이메일 인증코드를 발송하였습니다.", Toast.LENGTH_SHORT).show();
+                                }
+                            };
+                            task.execute();
+                            code = emailsend.getEmailCode();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Toast.makeText(ActivityChangePass.this, "잘못된 이메일 입니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
 
         // 인증코드 확인
         email_check_code.addTextChangedListener(new TextWatcher() {
